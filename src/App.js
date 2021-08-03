85f48e53505221a9fd7ed413602278e29493ace4
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-03 19:09:52 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react"
-import Potato from "./Potato";
 
+function Food({ fav }) {
+    return (
+        <h1>I like { fav }</h1>
+    )
+}
 function App() {
   return (
       <div>
         <h1>hello</h1>
-          <Potato />
+          <Food fav="kimchi"/>
+          <Food fav="ramen"/>
+          <Food fav="choco"/>
+          <Food fav="meat"/>
       </div>
   );
 }
