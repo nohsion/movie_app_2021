c5f3727f376002fef96a9075936c0283c2998c59
Sion Noh <shtldhs123@ajou.ac.kr>
2021-08-04 19:03:03 +0900

    fix CSS

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,17 +3,21 @@ import React from "react";
 
 class Detail extends React.Component {
     componentDidMount() {
-        const { location, history } = this.props
+        const {location, history} = this.props
         if (location.state === undefined) {
             history.push("/")
         }
     }
 
     render() {
-        const { location } = this.props
-        return (
-            <span>{location.state.title}</span>
-        )
+        const {location} = this.props
+        if (location.state) {
+            return (
+                <span>{location.state.title}</span>
+            )
+        }
+        else return null
+
     }
 }
 
